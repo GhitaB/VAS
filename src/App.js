68dcc327ae2ba74c2fc65988c4963499ecf88211
GhitaB <ghitabizau27@gmail.com>
2021-10-31 03:45:43 +0200

    Use SASS.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import './vas-theme.scss';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <div className="container">
+        <div className="box1">
+          <p>Test</p>
+        </div>
+        <div className="box2">
+          <p>Test</p>
+        </div>
+        <div className="box3">
+          <p>Test</p>
+        </div>
+        <div className="box4">
+          <p>Test</p>
+        </div>
+        <div className="box5">
+          <p>Test</p>
+        </div>
+      </div>
     </div>
   );
 }
