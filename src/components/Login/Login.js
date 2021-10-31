69a4fd91c9c757719016b380bab9c34974d14339
GhitaB <ghitabizau27@gmail.com>
2021-10-31 10:48:13 +0200

    Login form and demo validation.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,27 +1,55 @@
 import { useAtom } from 'jotai';
 import { isLoggedInAtom } from './state';
 import React, { useEffect } from 'react';
+import { useRef, useState } from "react";
 
-const validation = (user, password) => {
-  const isValid = true; // TODO...
-
-  return isValid;
+const validate = (user, password) => {
+  return (user === 'admin' && password == '123456'); // TODO haha, we are safe now
 }
 
 export default function Login() {
-  const [, setLoggedIn] = useAtom(isLoggedInAtom);
-  const user = 'User';
-  const password = '123456';
+  const [isLoggedIn, setLoggedIn] = useAtom(isLoggedInAtom);
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
 
-  useEffect(() => {
-    if (validation(user, password)) {
+  // useEffect(() => {
+  //   if (validation(user, password)) {
+  //     setLoggedIn(true);
+  //   } else {
+  //     setLoggedIn(false);
+  //   }
+  // });
+
+  const handleUserInputChange = (e) => {
+    setUser(e.target.value);
+  }
+
+  const handlePasswordInputChange = (e) => {
+    setPassword(e.target.value);
+  }
+
+  const loginValidation = (e) => {
+    if(validate(user, password)) {
       setLoggedIn(true);
     } else {
       setLoggedIn(false);
     }
-  });
+  }
 
   return (
-    <p>Welcome!</p>
+    <div className="login-form">
+      <h3>Login</h3>
+      {!isLoggedIn ? (
+        <div className="login-info">
+          <label for="user">Username:</label>
+          <input type="text" id="user" name="user" value={ user } onChange={ handleUserInputChange } />
+          <label for="password">Password:</label>
+          <input type="password" id="password" name="password" value={ password } onChange={ handlePasswordInputChange } />
+          <button onClick={ loginValidation }>Login</button>
+        </div>
+      ) : (
+        <p>Welcome!</p>
+      )}
+    </div>
   );
 }
