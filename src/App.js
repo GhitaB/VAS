04e51f596fa85e42252e806a770fc0dd3883cf07
GhitaB <ghitabizau27@gmail.com>
2021-10-31 06:52:42 +0200

    Use router.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './vas-theme.scss';
 import { appConfig } from './config';
 import logo from './images/logo200x200.png';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   console.log(appConfig);
@@ -11,6 +12,38 @@ function App() {
         <img className="logo" src={logo} alt="logo" />
         <h1>{appConfig.name}</h1>
         <p>{appConfig.description}</p>
+
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/users">Users</Link>
+                </li>
+              </ul>
+            </nav>
+
+            {/* A <Switch> looks through its children <Route>s and
+                renders the first one that matches the current URL. */}
+            <Switch>
+              <Route path="/about">
+                ABOUT here
+              </Route>
+              <Route path="/users">
+                USERS here
+              </Route>
+              <Route path="/">
+                HOME here
+              </Route>
+            </Switch>
+          </div>
+        </Router>
 
         <div className="box1">
           <p>Test</p>
